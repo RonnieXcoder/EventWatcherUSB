a062b53fc879e2d4697e5417c6e5950588e2df0a
RonnieXcoder <6543224+RonnieXcoder@users.noreply.github.com>
2024-06-13 22:37:11 +0300

    Add project files.

---
+++ EventWatcherUSB/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+using Microsoft.UI.Text;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace EventWatcherUSB
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private ManagementEventWatcher watcher;
+        private WqlEventQuery query;
+
+        public record File
+        {
+            public string Filename { get; set; }
+            public string FileType { get; set; }
+        }
+
+        private ObservableCollection<File> Contents { get; set; } = new ObservableCollection<File>();
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+
+            string driverName = string.Empty;
+
+            Contents.Clear();
+
+            query = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE " +
+                "TargetInstance ISA 'Win32_LogicalDisk'");
+
+            using (watcher = new ManagementEventWatcher(query))
+            {
+
+                StatusBlock.Text = "Waiting for USB device connection...\n";
+
+                watcher.EventArrived += async (sender, e) =>
+                {
+
+                    ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+
+                    if (instance != null)
+                    {
+
+
+                        DispatcherQueue.TryEnqueue(() =>
+                        {
+                            StatusBlock.Text += "USB device connected\n";
+
+                            });
+
+                        foreach (var property in instance.Properties)
+                        {
+
+                            if (property.Name == "Name") driverName = property.Value.ToString();
+
+                        }
+
+                        await ShowDeviceContentsAsync(driverName);
+
+                    }
+
+                };
+            }
+
+            watcher.Start();
+
+        }
+
+        public async Task ShowDeviceContentsAsync(string driverName)
+        {
+            try
+            {
+
+                DriveInfo drive = new DriveInfo(driverName);
+
+
+                DispatcherQueue.TryEnqueue(() =>
+                {
+                    Contents.Clear();
+                    StatusBlock.Text += $"Contents of the device {drive.Name}:\n";
+                });
+
+                DirectoryInfo directory = new DirectoryInfo(driverName);
+
+                await Task.Run(() =>
+                {
+                    foreach (var item in directory.GetFileSystemInfos())
+                    {
+                        DispatcherQueue.TryEnqueue(() =>
+                        {
+                            Contents.Add(new File
+                            {
+                                Filename = item.Name,
+                                FileType =
+                                (item is DirectoryInfo ? "Folder" : "File")
+                            });
+                        });
+                    }
+                });
+
+            }
+            catch (Exception ex)
+            {
+                DispatcherQueue.TryEnqueue(() =>
+                {
+                    StatusBlock.Text += $"Error while trying to display device contents: { ex.Message} ";
+                    });
+            }
+        }
+
+
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (watcher != null) watcher.Stop();
+            StatusBlock.Text = "";
+            Contents.Clear();
+        }
+
+    }
+}
